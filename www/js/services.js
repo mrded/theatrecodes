45d8469a3d7e361e4c87be7596503267ededce83
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-08-31 20:37:22 +0100

    Cleanup code

--- www/js/services.js
+++ www/js/services.js
@@ -4,7 +4,6 @@ angular.module('starter.services', [])
  * A simple example service that returns some data.
  */
 .factory('Codes', function($http, $q, $ionicLoading) {
-  var host = 'http://theatrecodes.co.uk';
   var db = {};
   
   document.addEventListener("deviceready", function() {
@@ -14,26 +13,20 @@ angular.module('starter.services', [])
     db.transaction(function(tx) {
       tx.executeSql("select name from sqlite_master where type='table' AND name='codes';", [], function(tx, res) {
         if (res.rows.length == 0) {
-          console.log('CREATE TABLE');
           $ionicLoading.show({template: 'Getting new codes'});
           
           db.executeSql('CREATE TABLE IF NOT EXISTS codes (id varchar(5) primary key, description text)');
   
-          console.log('GET NEW CODES');
-          $http.get(host + '/codes/').success(function(result) {
-            
+          $http.get('http://theatrecodes.co.uk/codes/').success(function(result) {
             db.transaction(function(tx) {
               angular.forEach(result, function(code) {
-                tx.executeSql("INSERT INTO codes (id, description) VALUES (?,?)", [code.name, code.description], function(tx, res) {
-                  console.log('INSERT', JSON.stringify(res.rows.insertId));
-                });
+                tx.executeSql("INSERT INTO codes (id, description) VALUES (?,?)", [code.name, code.description]);
               });
-            }, function(e) { console.log("ERROR", e.message); });
+            });
   
             $ionicLoading.hide();
           });
         }
-        else console.log('Load codes');
       });
     });
   }, false);
@@ -53,9 +46,7 @@ angular.module('starter.services', [])
       var codes = [];
       
       db.executeSql("select * from codes where description like '%{search}%' or id like '%{search}%';".replace('{search}', searchText), [], function(res) {
-        for (i = 0; i < res.rows.length; i++) {
-          codes.push(res.rows.item(i));
-        }
+        for (i = 0; i < res.rows.length; i++) codes.push(res.rows.item(i));
         return deferred.resolve(codes);
       });
       
